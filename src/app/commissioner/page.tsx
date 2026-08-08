d9fa9bf20e1783493246d2acb7fae6c2f844d006
MegaX99 <andrewfigura@gmail.com>
2026-08-08 11:41:24 -0700

    Add commissioner and owner dashboards

---
+++ src/app/commissioner/page.tsx
@@ -0,0 +1,8 @@
+import { CommissionerDashboard } from "@/components/commissioner-dashboard";
+import { DEFAULT_POOL_ID } from "@/lib/picks";
+
+export default async function CommissionerPage({ searchParams }: PageProps<"/commissioner">) {
+  const pool = (await searchParams).pool;
+  return <CommissionerDashboard poolId={typeof pool === "string" ? pool : DEFAULT_POOL_ID} />;
+}
+
