055178f4a36f5305d8f3f90efd971794a293aca5
MegaX99 <andrewfigura@gmail.com>
2026-08-08 12:15:14 -0700

    Add personalized home landing page

---
+++ src/app/picks/page.tsx
@@ -0,0 +1,7 @@
+import { PicksDashboard } from "@/components/picks-dashboard";
+
+export default async function PicksPage({ searchParams }: PageProps<"/picks">) {
+  const pool = (await searchParams).pool;
+  return <PicksDashboard poolId={typeof pool === "string" ? pool : undefined} />;
+}
+
